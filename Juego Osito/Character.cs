6ffdd02867e41696ee2e250336ab8cf7f6088ad0
Ifritdo <JulianFirpo316@hotmail.com>
2024-05-26 17:53:32 -0300

    Revert "Merge branch 'LevelController' into Develop"
    This reverts commit 9c21afabeafa12239e5d1477c1685baf89968640, reversing
    changes made to 7991f32dc7eeb4f56702ca33987bd3834a44ba51.

--- Juego Osito/Character.cs
+++ Juego Osito/Character.cs
@@ -6,7 +6,6 @@ using System.Net.Security;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Timers;
 using Tao.Sdl;
 
 namespace MyGame
@@ -17,7 +16,7 @@ namespace MyGame
         private Animation walk;
         private Animation lose;   
         private CharacterController controller;
-        private bool isLosing;
+        
 
         //transform.Position = new Vector2(200, 200);
 
@@ -26,7 +25,6 @@ namespace MyGame
         {
             controller = new CharacterController(transform);
             CreateAnimations();
-            isLosing = false;
         }
 
   
@@ -39,37 +37,24 @@ namespace MyGame
                 walkingTextures.Add(frame);
             }
             walk = new Animation("Walk", walkingTextures, 0.2f, true);
-            currentAnimation = walk; 
-        }
+            currentAnimation = walk;
 
-        public void NewAnimation()
-        {
             List<IntPtr> losingTextures = new List<IntPtr>();
             for (int i = 0; i < 15; i++)
             {
                 IntPtr frame = Engine.LoadImage($"assets/osito/lose/{i}.png");
                 losingTextures.Add(frame);
-
             }
-            lose = new Animation("Lose", losingTextures, 0.1f, true);
-            currentAnimation = lose;
-
-            if (losingTextures.Count <= 15)
-            {
-                isLosing = true;
-            }
+            lose = new Animation("Lose", losingTextures, 0.2f, true);
+            
         }
-
-
 
         public void ChangeAnimation() //cuando pierdo se cambia la animacion
         {
-            if (currentAnimation != walk) 
+            if (currentAnimation != null) 
             {
-               NewAnimation();   
-            }
-            isLosing = true;
-            currentAnimation = lose;
+                currentAnimation = lose;
+            } 
         }
 
 
@@ -92,9 +77,9 @@ namespace MyGame
          
         }*/
 
-        public void CheckCollisions()
+        private void CheckCollisions()
         {
-            for (int i = 0; i < LevelController.GameObjectList.Count; i++) 
+            for (int i = 0; i < LevelController.GameObjectList.Count; i++) //le voy a preguntar al profe sobre esto
             {
                 GameObject gameObject = LevelController.GameObjectList[i];
                 float distanceX = Math.Abs((gameObject.Transform.Position.x + (gameObject.Transform.Scale.x / 2)) - (transform.Position.x + (transform.Scale.x / 2)));
@@ -102,21 +87,9 @@ namespace MyGame
                 float sumHalfWidth = gameObject.Transform.Scale.x / 2 + transform.Scale.x / 2;
                 float sumHalfHeight = gameObject.Transform.Scale.y / 2 + transform.Scale.y / 2;
 
+
                 if (distanceX < sumHalfWidth && distanceY < sumHalfHeight)
                 {
-<<<<<<< Updated upstream
-                    Console.WriteLine("Colisión detectada!");
-                    LevelController.GameObjectList.Remove(gameObject); 
-                    ChangeAnimation();
-                    //DelayAfterLosing();
-                    GameManager.Instance.ChangeGameStatus(GameManager.GameStatus.lose);
-                    currentAnimation = walk; //aca hice q cuando pierdo, y quiero volver a jugar
-                                             //mi current animation cambie de perder a caminar
-                                             //puede q esto lo cambie mas adelante si genera conflicto con el delay
-=======
-                    
-
-                    // toco el pez
 
                     if (gameObject is IPickuppeable pickupobj) // Verifica si el objeto es un pez
                     {
@@ -124,41 +97,20 @@ namespace MyGame
                         LevelController.GameObjectList.RemoveAt(i); //ESTO SE TENDRIA QUE MODIFICAR CUANDO SE HAGA EL FACTORY. 
                     }
 
-                    // toco el arbol
-
                     if (gameObject is Obstacle)
                     {
                         Console.WriteLine("Colisión detectada con un obstaculo!");
                         LevelController.GameObjectList.Remove(gameObject);
-
-
                         ChangeAnimation();
-                        NewAnimation();
-                        isLosing = true;
-
-
-                        
-
-
-
-                        /*if (isLosing) 
-                        {
-
-                             GameManager.Instance.ChangeGameStatus(GameManager.GameStatus.lose);
-                        
-                        }*/
-
-
-                        //currentAnimation = walk; //aca hice q cuando pierdo, y quiero volver a jugar
-                        //mi current animation cambie de perder a caminar
+                        //DelayAfterLosing();
+                        GameManager.Instance.ChangeGameStatus(GameManager.GameStatus.lose);
+                        currentAnimation = walk; //aca hice q cuando pierdo, y quiero volver a jugar
+                                                 //mi current animation cambie de perder a caminar
                     }
->>>>>>> Stashed changes
-
-
 
                 }
 
-
+                
             }
         }
 
@@ -167,7 +119,6 @@ namespace MyGame
             base.Update();
             controller.GetInputs();
             CheckCollisions();
-
 
         }
 
